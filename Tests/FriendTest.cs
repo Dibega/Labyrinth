87ebfd9bf6bcb3c7e3687bca0afe192c6d400d16
Oleg <dev.oleg.sidorov@gmail.com>
2021-01-22 01:12:11 +0300

    Update test

--- Tests/FriendTest.cs
+++ Tests/FriendTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Labyrinth.Game;
 
 namespace Labyrinth.Tests
@@ -16,10 +11,9 @@ namespace Labyrinth.Tests
         {
             Friend friend = new Friend(new PointLabirinth(0,0),new IInteractable[,] { { null } });
             Player player = new Player(new ControlStyleMove(new IInteractable[,] { { null } }));
+            int hp = player.HealPoints;
             friend.Interact(player);
-            int i = 0;
-            Assert.That(i, Is.Not.EqualTo(player.HealPoints));
-            //Assert.That(player.HealPoints, Is.Not.EqualTo(player.HealPoints));
+            Assert.That(hp, Is.Not.EqualTo(player.HealPoints));
         }
     }
 }
